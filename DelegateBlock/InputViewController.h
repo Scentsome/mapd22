20bee17d7892223b2520811f10c87f85ff3b5265
Michael Pan <scentsome@gmail.com>
2018-03-01 13:49:59 +0800

    Impletment block version

---
+++ DelegateBlock/InputViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputViewController.h
+//  DelegateBlock
+//
+//  Created by Michael on 01/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputViewController : UIViewController
+
+@end
