b8bcd2ccc8947ba4ac425006d9d3e2ec70fa986c
Michael Pan <scentsome@gmail.com>
2018-03-01 16:28:28 +0800

    add container view

--- ReviewMVC_swift/ReviewMVC_swift/GrayViewController.swift
+++ ReviewMVC_swift/ReviewMVC_swift/GrayViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class GrayViewController: UIViewController {
-
+class GrayViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView2: UITableView!
+    
+    @IBOutlet weak var tableView1: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.tableView1 == tableView {
+            return 5
+        }
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        if tableView == self.tableView1 {
+            cell.textLabel?.text = "1000"
+        }else {
+            cell.textLabel?.text = "2000"
+        }
+        
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
