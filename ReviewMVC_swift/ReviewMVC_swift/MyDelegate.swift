71327f7947d5a01b00f3af06eb725ec717db295a
Michael Pan <scentsome@gmail.com>
2018-03-01 15:54:18 +0800

    Create MVC review project

---
+++ ReviewMVC_swift/ReviewMVC_swift/MyDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  MyDelegate.swift
+//  ReviewMVC_swift
+//
+//  Created by Michael on 01/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class MyDelegate: NSObject {
+
+}
---
+++ ReviewMVC_swift/ReviewMVC_swift/view/MyLabel.swift
@@ -0,0 +1,21 @@
+//
+//  MyLabel.swift
+//  ReviewMVC_swift
+//
+//  Created by Michael on 01/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class MyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
