17631f1beff86b0be8c7fadcdd60c9a8b0fd74f3
Michael Pan <scentsome@gmail.com>
2018-03-01 10:53:58 +0800

    Add Delegate Practice

---
+++ DelegatePractice/DelegatePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegatePractice
+//
+//  Created by Michael on 01/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegatePractice/DelegatePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegatePractice
+//
+//  Created by Michael on 01/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
