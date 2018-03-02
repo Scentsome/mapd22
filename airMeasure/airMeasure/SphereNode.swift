d4ba92c0981439731524065c028e0db988e8d0c3
Michael Pan <scentsome@gmail.com>
2018-03-02 16:33:44 +0800

    AR Measure

---
+++ airMeasure/airMeasure/SphereNode.swift
@@ -0,0 +1,13 @@
+//
+//  SphereNode.swift
+//  airMeasure
+//
+//  Created by Michael on 02/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class SphereNode: SCNNode {
+
+}
