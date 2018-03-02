a5150d86ba2a67618b44788d4325a9d696196f31
Michael Pan <scentsome@gmail.com>
2018-03-02 11:32:53 +0800

    Test spring effect

--- AnimateDemo/AnimateDemo/ViewController.swift
+++ AnimateDemo/AnimateDemo/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginLabel:UILabel!
+    
+    
+    
     @IBOutlet weak var accountField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func viewWillAppear(_ animated: Bool) {
-        let toRight = CABasicAnimation(keyPath: "position.x")
+        let toRight = CASpringAnimation(keyPath: "position.x")
+        toRight.damping = 10.0
         toRight.fromValue = 0
         toRight.toValue = 200
-        toRight.duration = 0.5
+        toRight.duration = 1.0
+        toRight.delegate = self
+//        toRight.autoreverses = true
+
+        toRight.setValue(self.loginLabel.layer, forKey: "loginLayer")
         self.accountField.layer.add(toRight, forKey: nil)
     }
 
@@ -28,5 +38,23 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+//extension ViewController : UITableViewDataSource {
+//    
+//}
+
+extension ViewController : CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        
+        print("animation stop")
+        
+        let loginLayer:CALayer? = anim.value(forKey: "loginLayer") as? CALayer
+        let pulse = CABasicAnimation(keyPath: "transform.scale")
+        pulse.fromValue = 1.5
+        pulse.toValue = 1.0
+        pulse.duration = 0.5
+        loginLayer?.add(pulse, forKey: nil)
+    }
 }
 
