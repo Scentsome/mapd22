ea452823b5f171367500a587f7250a3773f47e0c
Michael Pan <scentsome@gmail.com>
2018-03-05 11:15:04 +0800

    Demo Search table view

---
+++ SearchDemo/SearchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchDemo
+//
+//  Created by Michael on 05/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
